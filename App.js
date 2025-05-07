0366e658174699d78b3355cb628f4390fe7888d5
karunakar <karunakar.kukatla@aauti.com>
2025-05-07 17:56:00 +0530

    jsx and functional components

--- App.js
+++ App.js
@@ -1,42 +1,17 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-//pure js injecting into html
-        // const heading = document.createElement("h1")
-        // heading.innerHTML = 'KK React'
-        // const root = document.getElementById("root")
-        // root.appendChild(heading)
 
-        {/* using react */}
-        const heading = React.createElement("h1", {id:"heading"}, "KK React")
 
-        //creating root to our library
-        const root =  ReactDOM.createRoot(document.getElementById("root"))
+const heading = React.createElement("h1",{},'Hello')
+const jsxHeading = <h1>Hello JSX</h1>
 
-// creating nested tags
-        const parent = React.createElement("div", 
-            {id:"parent"},
-            React.createElement("div",{id:"child"}, 
-            React.createElement("h1",{id:"heading"}, "I'm h1 tag"))
-        )
-// creating nested tags and more than two child in one  tag
-        const parent1 = React.createElement("div", 
-            {id:"parent"},
-            React.createElement("div",{id:"child"}, 
-            [React.createElement("h1",{id:"heading"}, "I'm h1 tag"),
-            React.createElement("h2",{id:"heading"}, "I'm h2 tag")
-            ])
-        )
+const MyComponent = ()=>{
+        return <div>
+                {jsxHeading}
+        </div>
+}
 
-// creating nested tags and more than two child in one tag
-const parent2 = React.createElement("div", 
-    {id:"parent"},
-    [React.createElement("div",{id:"child"}, 
-    [React.createElement("h1",{id:"heading"}, "I'm h1 tag"),
-    React.createElement("h2",{id:"heading"}, "I'm h2 tag")
-    ]), React.createElement("div",{id:"child"}, 
-        [React.createElement("h1",{id:"heading"}, "I'm h1 tag"),
-        React.createElement("h2",{id:"heading"}, "I'm h2 tag")
-        ])]
-)
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(parent2)
+root.render(<MyComponent/>)
+

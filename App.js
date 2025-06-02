df409d34c1337bf25a822753a68f858c4fb1e166
karunakar <karunakar.kukatla@aauti.com>
2025-06-02 18:54:11 +0530

    ep-5,6

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./src/Components/Header";
+import Body from "./src/Components/Body";
 
+const heading = React.createElement("h1", {}, "Hello");
+const jsxHeading = <h1>Hello JSX</h1>;
 
-const heading = React.createElement("h1",{},'Hello')
-const jsxHeading = <h1>Hello JSX</h1>
+const MyComponent = () => {
+  return <div>{jsxHeading}</div>;
+};
 
-const MyComponent = ()=>{
-        return <div>
-                {jsxHeading}
-        </div>
-}
+const AppLayout = () => {
+  return (
+    <div className="app-layout">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<MyComponent/>)
-
+root.render(<AppLayout />);
---
+++ src/Components/Header.js
@@ -0,0 +1,54 @@
+const Header = () => {
+  return (
+    <div
+      className="header"
+      style={{
+        display: "flex",
+        justifyContent: "space-between", // To align logo left and nav right
+        alignItems: "center", // Center vertically
+        padding: "0 20px", // Optional: for some padding on the sides
+        borderBottomWidth: 1,
+        borderBottomStyle: "solid",
+        borderBottomColor: "#f0f0f0",
+      }}
+    >
+      <div className="logo-container">
+        <img
+          src="https://img.freepik.com/free-icon/burger_318-532444.jpg"
+          alt="logo"
+          className="logo"
+          style={{
+            height: "50px", // Optional: control the logo size
+          }}
+        />
+      </div>
+      <div
+        className="nav-container"
+        style={{
+          display: "flex",
+          flexDirection: "column", // Make the list a column
+          alignItems: "flex-end", // Align the list to the right
+        }}
+      >
+        <ul
+          style={{
+            listStyleType: "none",
+            padding: 0,
+            margin: 0,
+            display: "flex",
+            flexDirection: "row", // Make list items appear vertically
+            alignItems: "flex-end", // Align list items to the right
+            columnGap: 20,
+          }}
+        >
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
